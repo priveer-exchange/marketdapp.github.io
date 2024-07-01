8fa22a4d1a3e1928e52a24e60b8608e91449f27d
Max Mercuriev <max@yogi.pw>
2024-07-01 10:46:19 +0800

    create deal form with signing

--- src/views/Offer.jsx
+++ src/views/Offer.jsx
@@ -1,9 +1,16 @@
-import {Await, Link, useAsyncValue, useLoaderData} from "react-router-dom";
-import {Avatar, Breadcrumb, Button, Descriptions, Form, Input, List, Modal, Select, Skeleton, Space, Tag} from "antd";
+import {Await, Link, useLoaderData, useNavigate} from "react-router-dom";
+import {Breadcrumb, Button, Descriptions, Form, Input, message, Skeleton} from "antd";
 import React, {useState} from "react";
+
+import {Market} from "../js/contracts.js";
+import {useSDK} from "@metamask/sdk-react";
+import {ethers} from "ethers";
 
 export default function Offer() {
     const { data } = useLoaderData();
+    const { sdk, connected, connecting, provider, chainId } = useSDK();
+    const [ lockButton, setLockButton ] = useState(false);
+    const navigate = useNavigate();
 
     function title(offer) {
         let formatter = new Intl.NumberFormat('en-US', {
@@ -16,8 +23,27 @@ export default function Offer() {
         return formatter.format(offer.price);
     }
 
+    async function createDeal(offer, values) {
+        console.log(offer, values);
+        setLockButton(true);
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        const market = await Market.connect(await provider.getSigner());
+        market.createDeal(offer.id, values['fiatAmount'], values['paymentInstructions'] ?? '').then((tx) => {
+            message.info('Deal submitted. You will be redirected shortly.');
+            tx.wait().then((receipt) => {
+                receipt.logs.forEach(log => {
+                    const DealCreated = Market.interface.parseLog(log);
+                    if (DealCreated) {
+                        setLockButton(false);
+                        navigate('/trade/deal/'+DealCreated.args[2]);
+                    }
+                });
+            });
+            console.log(tx);
+        });
+    }
+
     return (
-        <>
         <React.Suspense fallback={<Skeleton active />}>
             <Await resolve={data}>
                 {(offer) => (
@@ -36,16 +62,27 @@ export default function Offer() {
                         {label: 'Terms', children: offer.terms},
                     ]}
                 />
-                <Form>
-                    <Input placeholder={"Crypto Amount"}/>
-                    <Input placeholder={"Fiat Amount"}/>
-                    <Input type={"textarea"} placeholder={"Payment instructions"}/>
-                    <Button type={"primary"}>Submit</Button>
+                <Form autoComplete={"off"}
+                      onFinish={(values) => createDeal(offer, values)}
+                >
+                    <Form.Item name={"tokenAmount"}>
+                        <Input placeholder={"Crypto Amount"}/>
+                    </Form.Item>
+                    <Form.Item name={"fiatAmount"} rules={[{required: true, message: "Required"}]}>
+                        <Input placeholder={"Fiat Amount"}/>
+                    </Form.Item>
+                    <Form.Item name={"paymentInstructions"}>
+                        <Input.TextArea placeholder={"Payment instructions"}/>
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type={"primary"} htmlType="submit"
+                            loading={lockButton}
+                        >Submit</Button>
+                    </Form.Item>
                 </Form>
                 </>
                 )}
             </Await>
         </React.Suspense>
-        </>
     );
 }
