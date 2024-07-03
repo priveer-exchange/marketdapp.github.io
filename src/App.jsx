cc625e9fe6f4132a4d0ec251940199330b6fd2f0
Max Mercuriev <max@yogi.pw>
2024-07-03 08:05:32 +0800

    use grid for header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, {Suspense} from 'react'
 import {Await, generatePath, Link, Outlet, useLoaderData, useParams} from "react-router-dom";
-import {Flex, Layout, Menu, Skeleton} from "antd";
+import {Button, Col, Flex, Layout, Menu, Row, Skeleton} from "antd";
 import Inventory from "./components/Inventory.jsx";
 import WalletMenu from "./components/WalletMenu.jsx";
 import {useWalletProvider} from "./hooks/useWalletProvider";
@@ -9,6 +9,7 @@ const {Header, Content, Sider} = Layout;
 
 export default function App() {
     const { inventory } = useLoaderData();
+    const { selectedAccount } = useWalletProvider();
 
     const top = [
         {
@@ -25,20 +26,22 @@ export default function App() {
     <>
     <Layout>
         <Header style={{paddingLeft: 0}}>
-            <Layout>
-                <Flex style={{padding: '0 10px'}}><Link to={"/"}>Fiat D.app</Link></Flex>
-                <Content>
-                    <Menu
-                        mode={"horizontal"}
+            <Row>
+                <Col flex={"100px"}><Link to={"/"}>Fiat D.app</Link></Col>
+                <Col flex={"auto"}>
+                    <Menu mode={"horizontal"}
                         theme={"dark"}
                         items={top}
                         defaultSelectedKeys={['sell']}
                     />
-                </Content>
-                <Sider align={"end"}>
+                </Col>
+                <Col flex={"auto"}>
+                    {selectedAccount && <Button><Link to={'/new'}>Create Offer</Link></Button>}
+                </Col>
+                <Col flex={"100px"}>
                     <WalletMenu />
-                </Sider>
-            </Layout>
+                </Col>
+            </Row>
         </Header>
         <Content>
             <Suspense fallback={<Skeleton paragraph={false} active />}>
