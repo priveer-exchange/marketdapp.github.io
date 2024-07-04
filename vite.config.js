03f032316be85e56e921738e77505829428180de
Max Mercuriev <max@yogi.pw>
2024-07-04 19:16:27 +0800

    enable absolute path @ loading of modules

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Set '@' to reference the 'src' directory
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
