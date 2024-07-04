74a0121379522fd8cee0b42fd1c500732bd78350
Max Mercuriev <max@yogi.pw>
2024-07-04 14:12:54 +0800

    Offer model for hydrations

---
+++ src/model/Offer.js
@@ -0,0 +1,29 @@
+export default class Offer
+{
+    /**
+     * @param response Response from the contract
+     */
+    static hydrate(response) {
+        const self = new Offer();
+        self.id     = Number(response[0]);
+        self.owner  = response[1];
+        self.isSell = response[2];
+        self.token  = response[3];
+        self.fiat   = response[4];
+        self.method = response[5];
+        self.rate   = Number(response[6]) / 10**4;
+        self.min    = Number(response[7]);
+        self.max    = Number(response[8]);
+        self.terms  = response[9];
+        return self;
+    }
+
+    /**
+     * @param marketPrice BigInt from Inventory contract
+     */
+    setPairPrice = (marketPrice) => {
+        const price = Number(marketPrice / 10000n) / 100;
+        this.price = (price * this.rate).toFixed(3);
+        return this;
+    }
+}
