456f36d51c3148e4da7e6f6bf62f0de564c19701
Max Mercuriev <max@yogi.pw>
2024-07-05 20:17:42 +0800

    signed() in context to invoke metamask only when signing

--- src/hooks/useContract.jsx
+++ src/hooks/useContract.jsx
@@ -3,6 +3,8 @@ import contracts from '../../contracts/deployed_addresses.json';
 import { abi as InventoryAbi} from '../../contracts/artifacts/Inventory_Inventory.json';
 import { abi as MarketAbi} from '../../contracts/artifacts/Market_ERC1967Proxy.json';
 import { abi as RepTokenAbi} from '../../contracts/artifacts/RepToken.json';
+import { abi as DealAbi} from '../../contracts/artifacts/Deal.json';
+import { abi as ERC20Abi } from '../../contracts/artifacts/ERC20.json';
 import {useEffect, useState} from "react";
 import {useWalletProvider} from "./useWalletProvider";
 
@@ -11,30 +13,40 @@ export const provider = new ethers.JsonRpcProvider('http://localhost:8545');
 export const InventoryContract = new ethers.Contract( contracts['Inventory#Inventory'], InventoryAbi, provider );
 export const MarketContract = new ethers.Contract( contracts['Market#ERC1967Proxy'], MarketAbi, provider );
 export const RepTokenContract = new ethers.Contract( contracts['Rep#RepToken'], RepTokenAbi, provider );
+export const TokenContract = new ethers.Contract( '0x', ERC20Abi, provider );
+
+export const DealContract = new ethers.Contract( '0x', DealAbi, provider );
 
 // eslint-disable-next-line react-refresh/only-export-components
 export function useContract()
 {
-    const { wallet, account } = useWalletProvider();
+    const { wallet } = useWalletProvider();
 
-    const [inventory, setInventory] = useState(InventoryContract);
-    const [market, setMarket] = useState(MarketContract);
-    const [repToken, setRepToken] = useState(RepTokenContract);
+    const inventory = InventoryContract;
+    const deal = DealContract;
+    const market = MarketContract;
+    const repToken = RepTokenContract;
+    const token = TokenContract;
+
+    const [signed, setSigned] = useState(() => ()=> console.warn('singer is not available'));
 
     useEffect(() => {
-        if (account) {
-            const provider = new ethers.BrowserProvider(wallet.provider);
-            provider.getSigner().then((signer) => {
-                setInventory(inventory.connect(signer));
-                setMarket(market.connect(signer));
-                setRepToken(repToken.connect(signer));
-            });
+        if (wallet) {
+            const getSigner = async (contract) => {
+                const provider = new ethers.BrowserProvider(wallet.provider);
+                const signer = await provider.getSigner();
+                return contract.connect(signer);
+            };
+            setSigned(() => getSigner)
         }
-    }, [account]);
+    }, [wallet]);
 
     return {
         inventory,
         market,
-        repToken
+        deal,
+        token,
+        repToken,
+        signed
     };
 }
