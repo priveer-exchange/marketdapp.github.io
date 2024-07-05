9f1e5e48bf242879318da5e635276f5fe235c62f
Max Mercuriev <max@yogi.pw>
2024-07-05 10:23:21 +0800

    optionally show avatar with address

--- src/components/Username.jsx
+++ src/components/Username.jsx
@@ -1,16 +1,28 @@
 import PropTypes from "prop-types";
 import ExplorerLink from "./ExplorerLink.jsx";
 import {formatAddress} from "../utils.js";
+import {Avatar, Space} from "antd";
 
-export default function Username({address})
+export default function Username({address, avatar = false})
 {
-    return (
+    const link = (
         <ExplorerLink address={address}>
             {formatAddress(address)}
         </ExplorerLink>
     );
+
+    if (avatar) {
+        return (
+            <Space>
+                <Avatar src={'https://effigy.im/a/'+address+'.svg'} draggable={false}/>
+                {link}
+            </Space>
+        )
+    }
+    else return link;
 }
 
 Username.propTypes = {
     address: PropTypes.string.isRequired,
+    avatar: PropTypes.bool,
 };
