430996c865e03790c5ac83e1ed8b757839eff28d
Max Mercuriev <max@yogi.pw>
2024-07-11 05:26:43 +0800

    use wagmi for chain selector

--- src/components/NetworkSelector.jsx
+++ src/components/NetworkSelector.jsx
@@ -1,66 +1,18 @@
-import {useWalletProvider} from "@/hooks/useWalletProvider";
-import {useEffect, useState} from "react";
 import {Select} from "antd";
+import {useChainId, useSwitchChain} from "wagmi";
 
 export default function NetworkSelector()
 {
-    const { account, wallet } = useWalletProvider();
-
-    const [chainId, setChainId] = useState();
-
-    const chains = {
-        "0x66eee": {
-            chainId: "0x66eee",
-            chainName: "Arbitrum Sepolia",
-            nativeCurrency: {
-                name: "Ethereum",
-                symbol: "ETH",
-                decimals: 18
-            },
-            rpcUrls: ['https://sepolia-rollup.arbitrum.io/rpc'],
-            blockExplorerUrls: ["https://sepolia.arbiscan.io/"]
-        },
-        "0x7a69": {
-            chainId: "0x7a69",
-            chainName: "Hardhat",
-            nativeCurrency: {
-                name: "Ethereum",
-                symbol: "ETH",
-                decimals: 18
-            },
-            rpcUrls: ['http://localhost:8545'],
-            blockExplorerUrls: ["http://localhost"]
-        }
-    };
-
-    function selectChain(id) {
-        wallet.provider.request({
-            method: "wallet_addEthereumChain",
-            params: [ chains[id] ]
-        }).then(() => location.reload());
-    }
-
-    useEffect(() => {
-        if (!wallet) return () => {};
-
-        wallet.provider.request({
-            "method": "eth_chainId",
-            "params": []
-        }).then(id => {
-            if (!chains[id]) {
-                selectChain(Object.keys(chains)[0]);
-            }
-            setChainId(id);
-        });
-    }, [wallet, account]);
+    const chainId = useChainId();
+    const { chains, switchChain } = useSwitchChain();
 
     const options = [];
-    for (let id in chains) {
+    chains.forEach((chain) => {
         options.push({
-            label: chains[id].chainName,
-            value: id
+            label: chain.name,
+            value: chain.id
         });
-    }
+    });
 
-    return (<Select style={{width: 150}} options={options} value={chainId} onSelect={selectChain} />);
+    return (<Select style={{width: 150}} options={options} value={chainId} onSelect={(id) => switchChain({chainId: id})} />);
 }
