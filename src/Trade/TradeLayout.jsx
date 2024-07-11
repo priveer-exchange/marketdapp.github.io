e500462388419e5e25d5e1ac234e57204d8c0c5c
Max Mercuriev <max@yogi.pw>
2024-07-11 18:46:24 +0800

    load inventory in component

--- src/Trade/TradeLayout.jsx
+++ src/Trade/TradeLayout.jsx
@@ -1,10 +1,61 @@
 import TokenNav from "@/Trade/TokenNav.jsx";
-import {Await, Outlet, useLoaderData} from "react-router-dom";
-import {Suspense} from "react";
+import {Await, Outlet} from "react-router-dom";
+import {Suspense, useEffect, useState} from "react";
 import {Skeleton} from "antd";
+import {ethers} from "ethers";
+import {useContract} from "@/hooks/useContract.jsx";
+
+function cached(key, promise) {
+    const now = new Date();
+    const cache = localStorage.getItem(key);
+    if (cache) {
+        return Promise.resolve(JSON.parse(cache).data);
+    } else {
+        return promise.then((res) => {
+            localStorage.setItem(key, JSON.stringify({ timestamp: now.getTime(), data: res }));
+            return res;
+        });
+    }
+}
 
 export default function TradeLayout() {
-    const inventory = useLoaderData();
+    const { Market } = useContract();
+
+    const [inventory, setInventory] = useState();
+
+    useEffect(() => {
+        setInventory({
+            tokens: cached('tokens', Market.getTokens().then(res => {
+                let tokens = {};
+                res.map(token => {
+                    token = {
+                        address: token[0],
+                        symbol: token[1],
+                        name: token[2],
+                        decimals: Number(token[3]),
+                    }
+                    tokens[token.symbol] = token;
+                });
+                return tokens;
+            })),
+            fiats: cached('fiats', Market.getFiats().then(res => {
+                return res.map(ethers.decodeBytes32String);
+            })),
+            methods: cached('methods', Market.getMethods().then(res => {
+                let methods = {};
+                res.map(method => {
+                    method = {
+                        name: method[0],
+                        group: Number(method[1]),
+                    }
+                    methods[method.name] = method;
+                });
+                return methods;
+            }))
+        });
+    }, []);
+
+    if (!inventory) return;
 
     return (
     <>
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import {createHashRouter, createRoutesFromElements, Navigate, Route, RouterProvi
 
 import './main.css'
 import {WalletProvider} from "./hooks/WalletProvider";
-import {offersLoader} from "@/Trade/Offers/offersLoader.js";
 import Layout from "./Layout";
 import Home from "./Home/index.jsx";
 import Profile from "@/Me/Profile.jsx";
@@ -12,7 +11,6 @@ import Deal from "@/Trade/Deal/Deal.jsx";
 import Offer from "@/Trade/Offer/Offer.jsx";
 import UserDeals from "@/Me/UserDeals.jsx";
 import OfferNew from "@/Trade/Offer/New/OfferNew.jsx";
-import inventoryLoader from "@/Trade/inventoryLoader.js";
 import TradeLayout from "@/Trade/TradeLayout.jsx";
 import Offers from "@/Trade/Offers/Offers.jsx";
 import {offerLoader} from "@/Trade/Offer/offerLoader.js";
@@ -25,14 +23,14 @@ import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 const router = createHashRouter( createRoutesFromElements(
     <Route element={<Layout />}>
         <Route index element={<Home/>} />
-        <Route path={"/trade"} element={<TradeLayout/>} loader={inventoryLoader}>
+        <Route path={"/trade"} element={<TradeLayout/>}>
             <Route index element={<Navigate to={"/trade/sell"} />} />
-            <Route path=":side/:token?/:fiat?/:method?" element={<Offers />} loader={offersLoader}/>
+            <Route path=":side/:token?/:fiat?/:method?" element={<Offers />}/>
             <Route path={"offer/:offerId"} element={<Offer/>} loader={offerLoader} />
             <Route path={"offer/new" } element={<OfferNew/>} />
             <Route path={"deal/:dealId"} element={<Deal/>} loader={dealLoader} />
         </Route>
-        <Route path={"/me"} element={<TradeLayout/>} loader={inventoryLoader}>
+        <Route path={"/me"} element={<TradeLayout/>}>
             <Route index element={<Profile />} />
             <Route path={"offers"} element={<UserOffers />}/>
             <Route path={"deals"} element={<UserDeals/>} />
