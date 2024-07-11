7fd2f2e80e04ff5ce4c1dd5c4262df4904ff1854
Max Mercuriev <max@yogi.pw>
2024-07-11 18:46:02 +0800

    chainId aware contracts

--- src/hooks/useContract.jsx
+++ src/hooks/useContract.jsx
@@ -1,63 +1,75 @@
-import { ethers } from "ethers";
-import contracts from '../../contracts/deployed_addresses.json';
-import { abi as MarketAbi} from '../../contracts/artifacts/Market.json';
-import { abi as RepTokenAbi} from '../../contracts/artifacts/RepToken.json';
-import { abi as DealAbi} from '../../contracts/artifacts/Deal.json';
-import { abi as OfferAbi} from '../../contracts/artifacts/Offer.json';
-import { abi as OfferFactoryAbi} from '../../contracts/artifacts/OfferFactory.json';
-import { abi as DealFactoryAbi } from '../../contracts/artifacts/DealFactory.json';
-import { abi as ERC20Abi } from '../../contracts/artifacts/ERC20.json';
-import {useEffect, useState} from "react";
-import {useWalletProvider} from "./useWalletProvider";
+import deployed from '../../contracts/addresses.json';
+import {abi as MarketAbi} from '../../contracts/artifacts/Market.json';
+import {abi as RepTokenAbi} from '../../contracts/artifacts/RepToken.json';
+import {abi as DealAbi} from '../../contracts/artifacts/Deal.json';
+import {abi as OfferAbi} from '../../contracts/artifacts/Offer.json';
+import {abi as OfferFactoryAbi} from '../../contracts/artifacts/OfferFactory.json';
+import {abi as DealFactoryAbi} from '../../contracts/artifacts/DealFactory.json';
+import {abi as ERC20Abi} from '../../contracts/artifacts/ERC20.json';
+import {useChainId, useClient, useConnectorClient} from "wagmi";
+import {BrowserProvider, ethers, FallbackProvider, JsonRpcProvider, JsonRpcSigner} from "ethers";
+import {useMemo} from "react";
 
-// eslint-disable-next-line react-refresh/only-export-components
-let provider;
-if (window.ethereum) {
-    provider = new ethers.BrowserProvider(window.ethereum);
-} else {
-    provider = new ethers.JsonRpcProvider('http://localhost:8545');
-}
-export { provider };
-export let MarketContract = new ethers.Contract( contracts['Market#Market'], MarketAbi, provider );
-export let RepTokenContract = new ethers.Contract( contracts['RepToken#RepToken'], RepTokenAbi, provider );
-export let OfferFactoryContract = new ethers.Contract( contracts['OfferFactory#OfferFactory'], OfferFactoryAbi, provider );
-export let DealFactoryContract = new ethers.Contract( contracts['DealFactory#DealFactory'], DealFactoryAbi, provider );
-export let TokenContract = new ethers.Contract( '0x', ERC20Abi, provider );
-export let OfferContract = new ethers.Contract( '0x', OfferAbi, provider );
-export let DealContract = new ethers.Contract( '0x', DealAbi, provider );
-
-// eslint-disable-next-line react-refresh/only-export-components
 export function useContract()
 {
-    const { wallet } = useWalletProvider();
+    const chainId = useChainId();
+    const client = useClient({chainId});
 
-    let deal = DealContract;
-    let market = MarketContract;
-    let repToken = RepTokenContract;
-    let token = TokenContract;
-    let offerFactory = OfferFactoryContract;
-    let dealFactory = DealFactoryContract;
-
-    const [signed, setSigned] = useState(() => ()=> console.warn('singer is not available'));
-
-    useEffect(() => {
-        if (wallet) {
-            const provider = new ethers.BrowserProvider(wallet.provider);
-            const getSigner = async (contract) => {
-                const signer = await provider.getSigner();
-                return contract.connect(signer);
-            };
-            setSigned(() => getSigner)
+    function clientToProvider(client) {
+        const { chain, transport } = client
+        const network = {
+            chainId: chain.id,
+            name: chain.name,
+            ensAddress: chain.contracts?.ensRegistry?.address,
         }
-    }, [wallet]);
+        if (transport.type === 'fallback') {
+            const providers = (transport.transports).map(
+                ({ value }) => new JsonRpcProvider(value?.url, network),
+            )
+            if (providers.length === 1) return providers[0]
+            return new FallbackProvider(providers)
+        }
+        return new JsonRpcProvider(transport.url, network)
+    }
+    useMemo(() => (client ? clientToProvider(client) : undefined), [client])
+
+    function clientToSigner(client) {
+        const { account, chain, transport } = client
+        const network = {
+            chainId: chain.id,
+            name: chain.name,
+            ensAddress: chain.contracts?.ensRegistry?.address,
+        }
+        const provider = new BrowserProvider(transport, network)
+        return new JsonRpcSigner(provider, account.address)
+    }
+    const { data: connector } = useConnectorClient({ chainId })
+    useMemo(() => (connector ? clientToSigner(connector) : undefined), [connector]);
+
+    const addresses = deployed[chainId];
+    const provider = clientToProvider(client);
+
+    const signed = async (contract) => {
+        const signer = clientToSigner(connector);
+        return contract.connect(signer);
+    };
+
+    const Market = new ethers.Contract(addresses['Market#Market'], MarketAbi, provider);
+    const OfferFactory = new ethers.Contract(addresses['OfferFactory#OfferFactory'], OfferFactoryAbi, provider);
+    const DealFactory = new ethers.Contract(addresses['DealFactory#DealFactory'], DealFactoryAbi, provider);
+    const RepToken = new ethers.Contract(addresses['RepToken#RepToken'], RepTokenAbi, provider);
+    const Deal = new ethers.Contract('0x', DealAbi, provider);
+    const Offer = new ethers.Contract('0x', OfferAbi, provider);
+    const Token = new ethers.Contract('0x', ERC20Abi, provider);
 
     return {
-        market,
-        offerFactory,
-        dealFactory,
-        deal,
-        token,
-        repToken,
-        signed
+        signed,
+        Market,
+        OfferFactory,
+        DealFactory,
+        RepToken,
+        Deal,
+        Offer,
+        Token
     };
 }
