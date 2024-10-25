56650c2ff2ffeb407356b08cfb55addd6d4dfbdb
Max Mercuriev <max@yogi.pw>
2024-10-26 03:34:47 +0500

    <Username> links to profile rather than explorer

--- src/components/Username.jsx
+++ src/components/Username.jsx
@@ -2,13 +2,12 @@ import PropTypes from "prop-types";
 import ExplorerLink from "./ExplorerLink.jsx";
 import {formatAddress} from "../utils.js";
 import {Avatar, Space} from "antd";
+import {Link} from "react-router-dom";
 
 export default function Username({address, avatar = false})
 {
     const link = (
-        <ExplorerLink address={address}>
-            {formatAddress(address)}
-        </ExplorerLink>
+        <Link to={"/profile/"+address}>{formatAddress(address)}</Link>
     );
 
     if (avatar) {
