1e5a6bf956b0d86bc776109bfcf415fc32f13605
Max Mercuriev <max@yogi.pw>
2024-10-28 03:06:05 +0500

    show rating next to user address

--- src/components/Username.jsx
+++ src/components/Username.jsx
@@ -3,11 +3,36 @@ import ExplorerLink from "./ExplorerLink.jsx";
 import {formatAddress} from "../utils.js";
 import {Avatar, Space} from "antd";
 import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {useContract} from "@/hooks/useContract.jsx";
 
 export default function Username({address, avatar = false})
 {
+    const { RepToken } = useContract();
+
+    const [ trades, setTrades ] = useState('0');
+    const [ rating, setRating ] = useState('0');
+
+    useEffect(() => {
+        if (RepToken) {
+            RepToken.ownerToTokenId(address).then(tokenId => {
+                RepToken.stats(tokenId).then(stats => {
+                    setTrades(String(stats[5]));
+                    const upvotes = Number(stats[1]);
+                    const downvotes = Number(stats[2]);
+                    const total = upvotes + downvotes;
+                    if (total === 0) {
+                        return setRating('- ');
+                    } else {
+                        setRating(String(upvotes / total * 100));
+                    }
+                });
+            });
+        }
+    });
+
     const link = (
-        <Link to={"/profile/"+address}>{formatAddress(address)}</Link>
+        <Link to={"/profile/"+address}>{formatAddress(address)} ({trades}; {rating}%)</Link>
     );
 
     if (avatar) {
