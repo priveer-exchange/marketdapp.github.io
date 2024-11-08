8a3baa6392f3f3688a76e8f5086d05fddcaae529
Max Mercuriev <max@yogi.pw>
2024-11-08 17:40:28 +0500

    polish deal controls component

--- src/pages/Trade/Deal/Controls.tsx
+++ src/pages/Trade/Deal/Controls.tsx
@@ -6,6 +6,7 @@ import LoadingButton from "components/LoadingButton.jsx";
 import {useContract} from "hooks/useContract";
 import Feedback from "./Feedback.jsx";
 import {useAccount} from "wagmi";
+import {equal} from "../../../utils";
 
 export default function Controls() {
     const {deal} = useDealContext();
@@ -14,42 +15,33 @@ export default function Controls() {
 
     if (!address) return;
 
-    async function call(methodName: string) {
+    /**
+     * Call the given method on Deal contract without params. Show a success message if provided.
+     *
+     * @param methodName
+     * @param successMessage
+     */
+    async function call(methodName: string, successMessage: string = '') {
         const contract = await signed(deal.contract);
         try {
             const tx = await contract[methodName]();
-            return await tx.wait();
+            await tx.wait();
+            if (successMessage) message.success(successMessage);
         } catch (e) {
             let parsed = deal.contract.interface.parseError(e.data);
             if (!parsed) parsed = Token.interface.parseError(e.data)
-            if (parsed) message.error(parsed.name)
             if (parsed.name === 'ERC20InsufficientBalance') {
-                message.error(`Not enough ${deal.token.symbol}. You have ${parsed.args[1]}`);
+                message.error(`Not enough ${deal.token.symbol}. You have ${parsed.args[1]}`); // FIXME divide by decimals
+            } else {
+                message.error(parsed.args[0])
             }
             throw e;
         }
     }
 
-    async function release() {
-        await call('release');
-        message.success('Released');
-    }
-
-    async function paid() {
-        await call('paid');
-        message.success('Paid');
-    }
-
-    async function cancel() {
-        await call('cancel');
-        message.success('Cancelled');
-    }
-
-    async function dispute() {
-        await call('dispute');
-        message.success('Disputed');
-    }
-
+    /**
+     * For sell deals owner funds contract before he can accept.
+     */
     async function accept() {
         if (deal.offer.isSell) {
             const t = await signed(Token.attach(deal.offer.token.address));
@@ -58,23 +50,13 @@ export default function Controls() {
                 await t.approve(Market.target, ethers.MaxUint256);
             }
         }
-        return call('accept').then(() => {
-            message.success('Accepted');
-        });
+        return call('accept',  'Accepted');
     }
 
-    function isOwner() {
-        return address.toLowerCase() === deal.offer.owner.toLowerCase();
-    }
-    function isTaker() {
-        return address.toLowerCase() === deal.taker.toLowerCase();
-    }
-    function isBuyer() {
-        return (deal.offer.isSell && isTaker()) || (!deal.offer.isSell && isOwner());
-    }
-    function isSeller() {
-        return (deal.offer.isSell && isOwner()) || (!deal.offer.isSell && isTaker());
-    }
+    const isOwner = () => equal(address, deal.offer.owner);
+    const isTaker = () => equal(address, deal.taker);
+    const isBuyer = () => (deal.offer.isSell && isTaker()) || (!deal.offer.isSell && isOwner());
+    const isSeller = () => (deal.offer.isSell && isOwner()) || (!deal.offer.isSell && isTaker());
 
     const State = {
         Initiated: 0,
@@ -86,44 +68,79 @@ export default function Controls() {
         Resolved: 6,
         Completed: 7
     };
+
+    const action = {
+        countAccept:    <span>Waiting for acceptance: <Statistic.Countdown value={deal.allowCancelUnacceptedAfter}/></span>,
+        countPaid:      <span>Waiting for payment: <Statistic.Countdown value={deal.allowCancelUnpaidAfter}/></span>,
+        countCancel:    <span>Cancel in <Statistic.Countdown value={deal.allowCancelUnpaidAfter}/></span>,
+        accept:         <LoadingButton type={"primary"} onClick={accept}>Accept</LoadingButton>,
+        paid:           <LoadingButton type={"primary"} onClick={() => call('paid', 'Paid')}>Paid</LoadingButton>,
+        release:        <LoadingButton type={"primary"} onClick={() => call('release', 'Released')}>Release</LoadingButton>,
+        dispute:        <LoadingButton danger onClick={() => call('dispute', 'Disputed')}>Dispute</LoadingButton>,
+        cancel:         <LoadingButton danger onClick={() => call('cancel', 'Canceled')}>Cancel</LoadingButton>,
+    };
     const controls = [];
-    // accept
-    if (deal.state === State.Initiated && isOwner()) {
-        controls.push(<LoadingButton type={"primary"} onClick={accept}>Accept</LoadingButton>);
-    }
 
-    // cancel
-    switch (true) {
-        case isOwner() && deal.state === State.Initiated:
-        case isBuyer() && deal.allowCancelUnpaidAfter < new Date():
-        case isBuyer() && deal.state > State.Initiated:
-        case isSeller() && deal.state < State.Paid && deal.allowCancelUnpaidAfter < new Date():
-            controls.push(<LoadingButton danger onClick={cancel}>Cancel</LoadingButton>);
+    // TODO dispute timings so that it can't be started right away
+    switch(true) {
+        case deal.state === State.Initiated:
+            if (isOwner()) {
+                controls.push(action.accept);
+                controls.push(action.cancel);
+            }
+            // FIXME acceptance time in contract is now 6 hours!! somewhere is timezone shift I guess
+            if (isTaker()) {
+                if (deal.allowCancelUnacceptedAfter > new Date()) {
+                    controls.push(action.countAccept);
+                } else {
+                    controls.push(action.cancel);
+                }
+            }
             break;
 
-        case isTaker() && deal.allowCancelUnacceptedAfter > new Date():
-            controls.push(<span>Waiting for acceptance: <Statistic.Countdown value={deal.allowCancelUnacceptedAfter} /></span>);
+        case deal.state === State.Accepted:
+            console.error('Accepted but not funded.') // FIXME UI response
             break;
 
-        case isSeller() && deal.state < State.Paid && deal.allowCancelUnpaidAfter > new Date():
-            controls.push(<span>Cancel in <Statistic.Countdown value={deal.allowCancelUnpaidAfter} /></span>);
+        case deal.state === State.Funded:
+            if (isSeller()) {
+                if (deal.allowCancelUnpaidAfter > new Date()) {
+                    controls.push(action.countPaid);
+                } else {
+                    controls.push(action.cancel);
+                }
+            }
+            if (isBuyer()) {
+                controls.push(action.paid);
+                controls.push(action.cancel);
+            }
             break;
-    }
 
-    // paid
-    if (deal.state === State.Funded && isBuyer()) {
-        controls.push(<LoadingButton type={"primary"} onClick={paid}>Paid</LoadingButton>);
-    }
+        case deal.state === State.Paid:
+            if (isSeller()) {
+                controls.push(action.release);
+                controls.push(action.dispute);
+            }
+            if (isBuyer()) {
+                controls.push(action.dispute);
+                controls.push(action.cancel);
+            }
+            break;
 
-    // release
-    if (deal.state > State.Funded && deal.state < State.Cancelled && isSeller()) {
-        controls.push(<LoadingButton type={"primary"} onClick={release}>Release</LoadingButton>);
-    }
+        case deal.state === State.Disputed:
+            if (isSeller()) {
+                controls.push(action.release);
+                controls.push(action.cancel);
+            }
+            if (isBuyer()) {
+                controls.push(action.cancel);
+            }
+            break;
 
-    // dispute
-    switch (true) {
-        case deal.state >= State.Paid && deal.state < State.Disputed:
-            controls.push(<LoadingButton danger onClick={dispute}>Dispute</LoadingButton>);
+        case deal.state === State.Cancelled:
+        case deal.state === State.Resolved:
+        case deal.state === State.Completed:
+            break;
     }
 
     if (deal.state < State.Completed) {
