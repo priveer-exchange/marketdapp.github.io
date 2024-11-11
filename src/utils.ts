f5cbc5ca2438c534f79e6628c6c716ec1a652756
Max Mercuriev <max@yogi.pw>
2024-11-11 12:53:17 +0500

    shorten address first part to 4 symbols

--- src/utils.ts
+++ src/utils.ts
@@ -4,7 +4,7 @@ export const formatChainAsNum = (chainIdHex) => {
 
 export const formatAddress = (addr) => {
     const upperAfterLastTwo = addr.slice(0,2) + addr.slice(2)
-    return `${upperAfterLastTwo.substring(0, 7)}...${upperAfterLastTwo.substring(38)}`
+    return `${upperAfterLastTwo.substring(0, 6)}...${upperAfterLastTwo.substring(38)}`
 }
 
 export const formatMoney = (currency, amount) => {
